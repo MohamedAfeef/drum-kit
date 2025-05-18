235ff77d07b7549ac8506e963aa34112d3d195d2
afeef <afeefmrm@gmail.com>
2025-05-18 14:42:26 +0530

    added bacground image

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var numberOfButtons = document.querySelectorAll(".drum").length;
 
 for (i = 0; i < numberOfButtons; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", function (){
-        alert('asd');
+        var audio = new Audio("sounds/tom-1.mp3");
+        audio.play();
     })
 }
